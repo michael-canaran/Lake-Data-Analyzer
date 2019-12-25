b0539cd32563b7b7173f9593801920fad09af0b9
michael-canaran <58964071+michael-canaran@users.noreply.github.com>
2019-12-25 01:21:11 -0500

    Add files via upload

--- LakeDataAnalyzer.c
+++ LakeDataAnalyzer.c
@@ -1,18 +1,4 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-/*Defining a lake structure*/
-struct Lake{
-	char name[30];
-	double averageTemp;
-	bool aboveorBelowAverage;
-	bool coldest;
-	bool warmest;
-	double winterAvg;
-	double summerAvg;
-	int daysFrozen;
-	int daysSwimmable;
-};
 
 /*Finds the date of occurence when given a row number, i*/
 int dateFinder(int i){
@@ -105,265 +91,265 @@ printf("%0.2d/%0.2d/2017\n",day,month);
 }
 
 /*Determines the amount of swimmable days (when lake temperature is over 20 degrees Celsius)*/
-int swimDays(double lakeTemps[365][6]){
-	int i, j, daysSwimmable;
+int swimDays(double lakeTemps[365][6], int column){
+	int i, j, daysSwimmable, swimmingDays;
 	for (j = 0; j < 6; j++){
 		daysSwimmable = 0;
 		for (i = 0; i < 365; i++){
-			if (lakeTemps[i][j] > 20.0){
+			if (lakeTemps[i][column] > 20.0){
 				daysSwimmable++;
 			}
+			swimmingDays = daysSwimmable;
 		}
 	}
 
-	return daysSwimmable;
+	return swimmingDays;
 }
 
 /*Determines the amount of days the lake is frozen (when lake temperature is below 0 degrees Celsius)*/
-int frozenDays(double lakeTemps[365][6]){
-	int i, j, daysFrozen;
-	for (j = 0; j < 6; j++){
+int frozenDays(double lakeTemps[365][6], int column){
+	int i, daysFrozen, frozen;
 		daysFrozen = 0;
-		for (i = 0; i < 365; i++){
-			if (lakeTemps[i][j] < 0.0){
-				daysFrozen++;
-			}
+		
+	for (i = 0; i < 365; i++){
+		if (lakeTemps[i][column] < 0.0){
+			daysFrozen+=1;
 		}
+		frozen = daysFrozen;
 	}
 
-	return daysFrozen;
+	return frozen;
 }
 
 /*Determines the summer water surface average of each lake*/
-double *summerAverage(double lakeTemps[365][6]){
-	int i,j;
-	double summerAvg[6], count = 0;
+double summerAverage(double lakeTemps[365][6], int column){
+	int i;
+	double summerAvg, count = 0.0;
 	
-	for (j = 0; j < 6; j++){
-		for (i = 171; i < 265; i++){
-			count += lakeTemps[i][j];
-		}
-		summerAvg[j] = count/(265-171);
+	for (i = 171; i < 265; i++){
+		count += lakeTemps[i][column];
 	}
+	summerAvg = count/(265-171);
 
 	return summerAvg;
 }
 
 /*Determines the winter water surface average of each lake*/
-double *winterAverage(double lakeTemps[365][6]){
-	int i,j;
-	double winterAvg[6], count = 0;
+double winterAverage(double lakeTemps[365][6], int column){
+	int i;
+	double winterAvg, count = 0;
 	
-	for (j = 0; j < 6; j++){
-		for (i = 0; i < 79; i++){
-			count += lakeTemps[i][j];
-		}
-		for (i = 354; i < 365; i++){
-			count += lakeTemps[i][j];
-		}
-		winterAvg[j] = count/(79+(365-354));
+	for (i = 0; i < 79; i++){
+		count += lakeTemps[i][column];
 	}
-
+	for (i = 354; i < 365; i++){
+		count += lakeTemps[i][column];
+	}
+	
+	winterAvg = count/(79+(365-354));
 	return winterAvg;
 }
 
-/*Determines the warmest lake*/
-bool *warmestLakes(double *warmestTemps){
-	int i,j,testValue;
-	bool warmest[6];
-
-	/*Initalize each array cell as false*/
-	for (i = 0; i < 6; i++){
-		warmest[i] = false;
-	}
+/*Prints the warmest lake*/
+int warmestLakes(double lakeTemps[365][6]){
+	int i, j, testValue, warmest;
+	double warmestTemps[6];
 
 	for (j = 0; j < 6; j++){
-		for (i = 5; i >= 0; i--){
-			testValue = warmestTemps[0];
-			if (testValue > warmestTemps[i]){
-				warmest[i] = true;
-			}
-		}
-	}
-
-	return warmest;
-}
-
-/*Determines the coldest lake*/
-bool *coldestLakes(double *coldestTemps){
-	int i,j,testValue;
-	bool coldest[6];
-
-	/*Initalize each array cell as false*/
-	for (i = 0; i < 6; i++){
-		coldest[i] = false;
-	}
-
-	for (j = 0; j < 6; j++){
-		for (i = 5; i >= 0; i--){
-			testValue = coldestTemps[0];
-			if (testValue > coldestTemps[i]){
-				coldest[i] = true;
-			}
-		}
-	}
-
-	return coldest;
-}
-
-double *warmestDay(double lakeTemps[365][6]){
-	int i,j,k;
-	double warmestTemps[6], testValue;
-
-	/*Finds and stores warmest temperature of each lake in an array*/
-	for (j = 0; j < 6; j++){
+	
+		/*Finds and stores warmest temperature of each lake*/
 		testValue = lakeTemps[0][j];
 		for (i = 0; i < 365; i++){
 			if (testValue < lakeTemps[i][j]){
 				testValue = lakeTemps[i][j];
-				k = j;
-				warmestTemps[k] = testValue;
+				warmestTemps[j] = testValue;
 			}
 		}
 	}
 
-	/*Checking the warmest temperatures against the lakeTemps array to determine the row/day which is then checked against the CalendarArray to display the date of occurence*/
+	/*Finds the lake with the highest temperature*/
 	for (j = 0; j < 6; j++){
-		switch (j){
-			case 0:
-				printf("\nLake Superior was at its warmest temperature of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			case 1:
-				printf("\nLake Michigan was at its warmest of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			case 2:
-				printf("\nLake Huron was at its warmest of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			case 3:
-				printf("\nLake Erie was at its warmest of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			case 4:
-				printf("\nLake Ontario was at its warmest of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			case 5:
-				printf("\nLake St. Clair was at its warmest of %0.2lf degrees Celsius on the following dates: \n", warmestTemps[j]);
-				break;
-			default:
-				printf("Error\n");
-				break;
-		}
-		for (i = 0; i < 365; i++){
-			if (warmestTemps[j] == lakeTemps[i][j]){
-				dateFinder(i);
+		for (i = 1; i < 6; i++){
+			if (warmestTemps[j] < warmestTemps[i]){
+				warmest = i;
 			}
 		}
 	}
 
-	return warmestTemps;
+	/*Prints which lake is the warmest and displays the highest temperature*/
+	switch (warmest){
+		case 0:
+			printf("\nLake Superior had the highest temperature of all the lakes of %0.2lf", warmestTemps[0]);
+			break;
+		case 1:
+			printf("\nLake Michigan had the highest temperature of all the lakes of %0.2lf", warmestTemps[1]);
+			break;
+		case 2:
+			printf("\nLake Huron had the highest temperature of all the lakes of %0.2lf", warmestTemps[2]);
+			break;
+		case 3:
+			printf("\nLake Erie had the highest temperature of all the lakes of %0.2lf", warmestTemps[3]);
+			break;
+		case 4:
+			printf("\nLake Ontario had the highest temperature of all the lakes of %0.2lf", warmestTemps[4]);
+			break;
+		case 5:
+			printf("\nLake St.Clair had the highest temperature of all the lakes of %0.2lf", warmestTemps[5]);
+			break;
+		default:
+			printf("Error\n");
+			break;
+	}
 }
 
-/*Finds and prints the coldest days of each lake*/ 
-double *coldestDay(double lakeTemps[365][6]){
-	int i,j,k;
-	double coldestTemps[6], testValue;
+/*Prints the coldest lake*/
+int coldestLakes(double lakeTemps[365][6]){
+	int i, j, testValue, coldest;
+	double coldestTemps[6];
 
-	/*Finds and stores coldest temperature of each lake in an array*/
 	for (j = 0; j < 6; j++){
+	
+		/*Finds and stores coldest temperature of each lake*/
 		testValue = lakeTemps[0][j];
 		for (i = 0; i < 365; i++){
 			if (testValue > lakeTemps[i][j]){
 				testValue = lakeTemps[i][j];
-				k = j;
-				coldestTemps[k] = testValue;
+				coldestTemps[j] = testValue;
 			}
 		}
 	}
 
-	/*Checking the coldest temperatures against the lakeTemps array to determine the row/day which is then checked against the CalendarArray to display the date of occurence*/
+	/*Finds the lake with the lowest temperature*/
 	for (j = 0; j < 6; j++){
-		switch (j){
-			case 0:
-				printf("\nLake Superior was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			case 1:
-				printf("\nLake Michigan was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			case 2:
-				printf("\nLake Huron was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			case 3:
-				printf("\nLake Erie was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			case 4:
-				printf("\nLake Ontario was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			case 5:
-				printf("\nLake St. Clair was at its coldest of %0.2lf degrees Celsius on the following dates: \n", coldestTemps[j]);
-				break;
-			default:
-				printf("Error\n");
-				break;
-		}
-		for (i = 0; i < 365; i++){
-			if (coldestTemps[j] == lakeTemps[i][j]){
-				dateFinder(i);
+		for (i = 1; i < 6; i++){
+			if (coldestTemps[j] > coldestTemps[i]){
+				coldest = i;
 			}
 		}
 	}
-	
-	return coldestTemps;
+
+	/*Prints which lake is the coldest and displays the lowest temperature*/
+	switch (coldest){
+		case 0:
+			printf("\nLake Superior had the lowest temperature of all the lakes of %0.2lf", coldestTemps[0]);
+			break;
+		case 1:
+			printf("\nLake Michigan had the lowest temperature of all the lakes of %0.2lf", coldestTemps[1]);
+			break;
+		case 2:
+			printf("\nLake Huron had the lowest temperature of all the lakes of %0.2lf", coldestTemps[2]);
+			break;
+		case 3:
+			printf("\nLake Erie had the lowest temperature of all the lakes of %0.2lf", coldestTemps[3]);
+			break;
+		case 4:
+			printf("\nLake Ontario had the lowest temperature of all the lakes of %0.2lf", coldestTemps[4]);
+			break;
+		case 5:
+			printf("\nLake St.Clair had the lowest temperature of all the lakes of %0.2lf", coldestTemps[5]);
+			break;
+		default:
+			printf("Error\n");
+			break;
+	}
 }
 
-/*Determines whether a lake is above or below the average temperature of all the lakes*/
-bool *aboveOrBelow (double averagedTemps[6]){
+/*Finds and prints the warmest days of each lake*/
+double warmestDay(double lakeTemps[365][6], int column){
 	int i;
-	double averageTemp = 0;
-	bool aboveOrBelow[6];
+	double warmestTemps, testValue;
+
+	/*Finds and stores warmest temperature of each lake*/
+	testValue = lakeTemps[0][column];
+	for (i = 0; i < 365; i++){
+		if (testValue < lakeTemps[i][column]){
+			testValue = lakeTemps[i][column];
+			warmestTemps = testValue;
+		}
+	}
+
+	/*Checking the warmest temperatures against the lakeTemps array to determine the row/day which is then sent to the dateFinder to display the date of occurence*/
+	printf("Experienced warmest temperature of %0.2lf degrees Celsius on:\n", warmestTemps);
+	for (i = 0; i < 365; i++){
+		if (warmestTemps == lakeTemps[i][column]){
+			dateFinder(i);
+		}
+	}
+}
+
+/*Finds and prints the coldest days of each lake*/ 
+double coldestDay(double lakeTemps[365][6], int column){
+	int i;
+	double coldestTemps, testValue;
+
+	/*Finds and stores coldest temperature of each lake*/
+	testValue = lakeTemps[0][column];
+	for (i = 0; i < 365; i++){
+		if (testValue > lakeTemps[i][column]){
+			testValue = lakeTemps[i][column];
+			coldestTemps = testValue;
+		}
+	}
+
+	/*Checking the coldest temperatures against the lakeTemps array to determine the row/day which is then sent to the dateFinder to display the date of occurence*/
+	printf("Experienced coldest temperature of %0.2lf degrees Celsius on:\n", coldestTemps);
+	for (i = 0; i < 365; i++){
+		if (coldestTemps == lakeTemps[i][column]){
+			dateFinder(i);
+		}
+	}
+}
+
+/*Determines and prints whether a lake is above or below the average temperature of all the lakes*/
+int aboveOrBelow (double lakeTemps[365][6], int column){
+	int i,j;
+	double averageTemp = 0.0, averagedLakeTemp = 0.0;
 
 	/*Finding the average temperature of all the lakes*/
-	for (i = 0; i < 6; i++){
-		averageTemp += averagedTemps[i];
+	for (j = 0; j < 6; j++){
+		for (i = 0; i < 365; i++){
+			averageTemp += lakeTemps[i][j];
+		}
 	}
 
-	averageTemp /= 6;
+	averageTemp /= i;
 
-	/*Determining if the average temperature of each lake is above or below the average of all lakes*/
-	for (i = 0; i < 6; i++){
-			if (averagedTemps[i] > averageTemp){
-				aboveOrBelow[i] = true;
-			}
-			else{
-				aboveOrBelow[i] = false;
-			}
+	/*Determining if the average temperature of the lake is above or below the average of all lakes*/
+	for (i = 0; i < 365; i++){
+		averagedLakeTemp += lakeTemps[i][column];
 	}
 
-	return aboveOrBelow;
+	averagedLakeTemp /= i;
 
+	if (averagedLakeTemp > averageTemp){
+		printf("The lake is above the average temperature of all the lakes, of %0.2lf degrees Celsius\n", averagedLakeTemp);
+	}
+	else{
+		printf("The lake is below the average temperature of all the lakes, of %0.2lf degrees Celsius\n", averagedLakeTemp);
+	}
 }
 
 /*Finds the average temperature of each lake*/
-double *averageTemp(double lakeTemps[365][6]){
+double averageTemp(double lakeTemps[365][6], int column){
 	int i,j;
-	double averagedTemps[6];
+	double averagedTemps;
 
 	/*Finding the average temperature of each column and assigning them to their resepective average variable*/
-	for (j = 0; j < 6; j++){
-		averagedTemps[j] = 0;
-		for (i = 0; i < 365; i++){
-			averagedTemps[j] += lakeTemps[i][j];
-		}
-		averagedTemps[j] /= i;
+	averagedTemps = 0;
+	for (i = 0; i < 365; i++){
+		averagedTemps += lakeTemps[i][column];
 	}
+	averagedTemps /= i;
 
 	return averagedTemps;	
 }
 
 /*Main function*/
 int main(void){
-	int i,j;
-	double table[365][8], lakeTemps[365][6], *averagedTemps, *warmestTemps, *coldestTemps, *winterAvg, *summerAvg;
-	bool *coldestLake, *warmestLake, *aboveOrBelowAverage;
+	int i, j;
+	double table[365][8], lakeTemps[365][6];
 
+	/*Opening the temperature text file with column headers removed*/
 	FILE*temps;
 	temps = fopen("temps.txt","r");
 	/*Index the temperature file and sort the data into an array*/
@@ -387,15 +373,86 @@ int main(void){
 	}
 
 	fclose(temps);
-	printf("**NOTICE**: ALL DATES ARE PRESENTED IN DAY/MONTH/YEAR FORMAT\n\n");
+	printf("**NOTICE**: ALL DATES ARE PRESENTED IN DAY/MONTH/YEAR FORMAT\n");
 	
-	/*Function calls*/
-	averagedTemps = averageTemp(lakeTemps);
-	aboveOrBelowAverage = aboveOrBelow(averagedTemps);
-	coldestTemps = coldestDay(lakeTemps);
-	warmestTemps = warmestDay(lakeTemps);
-	coldestLake = coldestLakes(coldestTemps);
-	warmestLake = warmestLakes(warmestTemps);
-	winterAvg = winterAverage(lakeTemps);
-	summerAvg = summerAverage(lakeTemps);
+	/*Printing out lake specific data*/
+	for (j = 0; j < 6; j++){
+		switch(j){
+			case 0:
+				printf("\nData for Lake Superior:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			case 1:
+				printf("\nData for Lake Michigan:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			case 2:
+				printf("\nData for Lake Huron:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			case 3:
+				printf("\nData for Lake Erie:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			case 4:
+				printf("\nData for Lake Ontario:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			case 5:
+				printf("\nData for Lake St.Clair:\n");
+				printf("Average temperature: %0.2lf degrees Celsius\n", averageTemp(lakeTemps, j));
+				printf("Average winter temperature: %0.2lf degrees Celsius\n", winterAverage(lakeTemps, j));
+				printf("Average summer temperature: %0.2lf degrees Celsius\n", summerAverage(lakeTemps, j));
+				printf("Days frozen: %d\n", frozenDays(lakeTemps, j));
+				printf("Swimmable days: %d\n", swimDays(lakeTemps, j));
+				aboveOrBelow(lakeTemps, j);
+				coldestDay(lakeTemps, j);
+				warmestDay(lakeTemps, j);
+				break;
+			default:
+				printf("Error\n");
+				break;
+		}
+	}
+
+	printf("\n\nGeneral information:\n");
+	coldestLakes(lakeTemps);
+	warmestLakes(lakeTemps);
+
 }
+
